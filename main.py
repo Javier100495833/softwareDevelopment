6a5bc7641879e90784f613b5293b60fc39f27b89
Jav163 <100495833@alumnos.uc3m.es>
2024-02-22 16:18:21 +0100

    ennove paolo gay

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-a = 2
-b = 3
-print(a+b)
-ennove = "me cago en todo"
+class Circle:
+    def __init__(self, r):
+        self.radius = r
+
+    def area(self):
+        return self.radius ** 2 * 3.14
+
+    def perimeter(self):
+        return 2 * self.radius * 3.14
---
+++ test.py
@@ -0,0 +1,6 @@
+from main import Circle
+
+class myTest:
+    def calculate_area_test(self):
+        obj = Circle()
+        assert (obj.radius(2.5) = )
